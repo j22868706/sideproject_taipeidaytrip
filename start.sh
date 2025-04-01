807d7bdbb38245a7f3a53915715c67fa6633265d
j22868706 <j22868706@gmail.com>
2025-03-31 22:05:29 -0500

    corrected nginx.conf and start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Gunicorn in daemon mode with Eventlet worker, access and error logs, and bind to 0.0.0.0:3000
-gunicorn -D -w 1 --worker-class eventlet --access-logfile gunicorn_access.log --error-logfile gunicorn_error.log -b 0.0.0.0:3000 app:app
+gunicorn -D -w 1 --worker-class eventlet --access-logfile gunicorn_access.log --error-logfile gunicorn_error.log -b 0.0.0.0:3000 app:app 
 
-# Start Nginx in foreground mode
+# Start Nginx in foreground mode (no daemon)
 nginx -g 'daemon off;'
